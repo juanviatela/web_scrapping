7e7aeeeacc4d97210a6c6c37cbfaffd77cbbf220
juanviatela <135143341+juanviatela@users.noreply.github.com>
2024-01-24 12:07:56 +0100

    Add files via upload

---
+++ vinyls_results.jl
@@ -0,0 +1,67 @@
+{"Title": "OBITUARY \u2013 SLOWLY WE ROT 2A MA NM.NM.", "Price": "80.00", "Format": "LP"}
+{"Title": "EXODUS \u2013 IMPACT IS IMMINENT 2A MA NM.NM.", "Price": "70.00", "Format": "LP"}
+{"Title": "SEPULTURA \u2013 ARISE 2A MA NM.NM.", "Price": "100.00", "Format": "LP"}
+{"Title": "METALLICA \u2013 \u2026AND JUSTICE FOR ALL 2A MA NM.NM.", "Price": "60.00", "Format": "2LP\u00a0"}
+{"Title": "MOTORHEAD \u2013 1916 2A MA NM.NM.", "Price": "60.00", "Format": "CD."}
+{"Title": "EVO \u2013 DURACION DE LO ETERNO 2A MA NM.NM.", "Price": "200.00", "Format": "LP"}
+{"Title": "OVERDOSE/SEPULTURA \u2013 SECULO XX/BESTIAL DEVASTATION 2A MA NM.VG+", "Price": "250.00", "Format": "LP"}
+{"Title": "DESTRUCTION \u2013 RELEASE FROM AGONY 2A MA NM.NM.", "Price": "35.00", "Format": "LP"}
+{"Title": "BAD RELIGION \u2013 GENERATOR 2A MA NM.NM.", "Price": "50.00", "Format": "CD."}
+{"Title": "EXCITER \u2013 LONG LIVE THE LOUD 2A MA NM.VG+.", "Price": "30.00", "Format": "LP"}
+{"Title": "SLAYER \u2013 SOUTH OF HEAVEN 2A MA NM.NM.", "Price": "80.00", "Format": "LP"}
+{"Title": "RAMONES \u2013 ROCKET TO RUSSIA 2A MA NM.NM.", "Price": "50.00", "Format": "LP"}
+{"Title": "RAMONES \u2013 RAMONES 2A MA NM.VG+.", "Price": "40.00", "Format": "LP"}
+{"Title": "BLUR \u2013 FOR TOMORROW 2A MA VG+.VG+.", "Price": "35.00", "Format": "LP"}
+{"Title": "KILLING JOKE \u2013 AN INCOMPLETE COLLECTION 2A MA NM.VG+", "Price": "250.00", "Format": "5LP"}
+{"Title": "TELEVISION \u2013 TELEVISION 2A MA NM.NM.", "Price": "150.00", "Format": "LP"}
+{"Title": "CRANES \u2013 WINGS OF JOY 2A MA VG+.VG+.", "Price": "25.00", "Format": "LP"}
+{"Title": "DEF LEPPARD \u2013 ADRENALIZE 2A MA NM.NM.", "Price": "35.00", "Format": "LP"}
+{"Title": "CURE \u2013 PORNOGRAPHY 2A MA NM.NM.", "Price": "300.00", "Format": "LP"}
+{"Title": "MILES DAVIS \u2013 AURA 2A MA NM.VG+", "Price": "40.00", "Format": "LP"}
+{"Title": "CURE \u2013 MIXED UP 2A MA NM.NM.", "Price": "40.00", "Format": "2LP\u00a0"}
+{"Title": "KREATOR \u2013 EXTREME AGGRESSION 2A MA NM.NM.", "Price": "50.00", "Format": "LP"}
+{"Title": "KREATOR \u2013 OUT OF THE DARK\u20262A MA NM.NM.", "Price": "40.00", "Format": "LP"}
+{"Title": "FLOTSAM AND JETSAM \u2013 NO PLACE FOR DISGRACE 2A MA NM.NM.", "Price": "35.00", "Format": "LP"}
+{"Title": "SACRED REICH \u2013 IGNORANCE 2A MA NM.NM.", "Price": "50.00", "Format": "LP"}
+{"Title": "SANGRE AZUL \u2013 EL SILENCIO DE LA NOCHE 2A MA VG+.VG+", "Price": "100.00", "Format": "LP"}
+{"Title": "RIGOR MORTIS \u2013 VETE AL INFIERNO 2A MA VG+.VG+", "Price": "35.00", "Format": "LP"}
+{"Title": "PET SHOP BOYS \u2013 DISCOGRAPHY 2A MA NM.VG+.", "Price": "80.00", "Format": "2LP\u00a0"}
+{"Title": "ATILA \u2013 REVIURE 2A MA VG+.VG.", "Price": "60.00", "Format": "LP"}
+{"Title": "JETHRO TULL \u2013 STAND UP 2A MA NM.NM.", "Price": "50.00", "Format": "LP"}
+{"Title": "JIMI HENDRIX \u2013 ELECTRIC LADYLAND 2A MA NM.VG+.", "Price": "80.00", "Format": "2LP\u00a0"}
+{"Title": "LED ZEPPELIN \u2013 LED ZEPPELIN II 2A MA NM.VG+", "Price": "100.00", "Format": "LP"}
+{"Title": "JUDAS PRIEST \u2013 RAM IT DOWN 2A MA NM.VG+.", "Price": "75.00", "Format": "LP"}
+{"Title": "AC/DC \u2013 FLY ON THE WALL 2A MA VG.VG.", "Price": "30.00", "Format": "LP"}
+{"Title": "SMITHS \u2013 THE BOY WITH THE THORN IN HIS SIDE 2A MA NM.NM.", "Price": "30.00", "Format": "LP"}
+{"Title": "W.A.S.P. \u2013 W.A.S.P. 2A MA VG+.VG.", "Price": "40.00", "Format": "LP"}
+{"Title": "LUIS ALBERTO SPINETTA \u2013 EXACTAS 2A MA VG+.VG+", "Price": "75.00", "Format": "LP"}
+{"Title": "13TH FLOOR ELEVATORS \u2013 EASTER EVERYWHERE 2A MA VG.VG+.", "Price": "45.00", "Format": "LP"}
+{"Title": "SONIC YOUTH \u2013 DAYDREAM NATION 2A MA VG+.VG+", "Price": "50.00", "Format": "2LP\u00a0"}
+{"Title": "CURE \u2013 MIXED UP 2A MA NM.VG+.", "Price": "50.00", "Format": "2LP\u00a0"}
+{"Title": "BON JOVI \u2013 KEEP THE FAITH 2A MA VG+.NM.", "Price": "50.00", "Format": "LP"}
+{"Title": "LAIBACH \u2013 LAIBACH 2A MA VG.VG+", "Price": "40.00", "Format": "LP"}
+{"Title": "QUEEN \u2013 QUEEN II 2A MA NM.NM.", "Price": "30.00", "Format": "LP"}
+{"Title": "OBITUARY \u2013 SLOWLY WE ROT 2A MA NM.NM.", "Price": "80.00", "Format": "LP"}
+{"Title": "EXODUS \u2013 IMPACT IS IMMINENT 2A MA NM.NM.", "Price": "70.00", "Format": "LP"}
+{"Title": "SEPULTURA \u2013 ARISE 2A MA NM.NM.", "Price": "100.00", "Format": "LP"}
+{"Title": "METALLICA \u2013 \u2026AND JUSTICE FOR ALL 2A MA NM.NM.", "Price": "60.00", "Format": "2LP\u00a0"}
+{"Title": "MOTORHEAD \u2013 1916 2A MA NM.NM.", "Price": "60.00", "Format": "CD."}
+{"Title": "OVERDOSE/SEPULTURA \u2013 SECULO XX/BESTIAL DEVASTATION 2A MA NM.VG+", "Price": "250.00", "Format": "LP"}
+{"Title": "EVO \u2013 DURACION DE LO ETERNO 2A MA NM.NM.", "Price": "200.00", "Format": "LP"}
+{"Title": "DESTRUCTION \u2013 RELEASE FROM AGONY 2A MA NM.NM.", "Price": "35.00", "Format": "LP"}
+{"Title": "BAD RELIGION \u2013 GENERATOR 2A MA NM.NM.", "Price": "50.00", "Format": "CD."}
+{"Title": "EXCITER \u2013 LONG LIVE THE LOUD 2A MA NM.VG+.", "Price": "30.00", "Format": "LP"}
+{"Title": "SLAYER \u2013 SOUTH OF HEAVEN 2A MA NM.NM.", "Price": "80.00", "Format": "LP"}
+{"Title": "RAMONES \u2013 ROCKET TO RUSSIA 2A MA NM.NM.", "Price": "50.00", "Format": "LP"}
+{"Title": "RAMONES \u2013 RAMONES 2A MA NM.VG+.", "Price": "40.00", "Format": "LP"}
+{"Title": "BLUR \u2013 FOR TOMORROW 2A MA VG+.VG+.", "Price": "35.00", "Format": "LP"}
+{"Title": "KILLING JOKE \u2013 AN INCOMPLETE COLLECTION 2A MA NM.VG+", "Price": "250.00", "Format": "5LP"}
+{"Title": "TELEVISION \u2013 TELEVISION 2A MA NM.NM.", "Price": "150.00", "Format": "LP"}
+{"Title": "CRANES \u2013 WINGS OF JOY 2A MA VG+.VG+.", "Price": "25.00", "Format": "LP"}
+{"Title": "DEF LEPPARD \u2013 ADRENALIZE 2A MA NM.NM.", "Price": "35.00", "Format": "LP"}
+{"Title": "CURE \u2013 PORNOGRAPHY 2A MA NM.NM.", "Price": "300.00", "Format": "LP"}
+{"Title": "MILES DAVIS \u2013 AURA 2A MA NM.VG+", "Price": "40.00", "Format": "LP"}
+{"Title": "CURE \u2013 MIXED UP 2A MA NM.NM.", "Price": "40.00", "Format": "2LP\u00a0"}
+{"Title": "KREATOR \u2013 EXTREME AGGRESSION 2A MA NM.NM.", "Price": "50.00", "Format": "LP"}
+{"Title": "KREATOR \u2013 OUT OF THE DARK\u20262A MA NM.NM.", "Price": "40.00", "Format": "LP"}
+{"Title": "FLOTSAM AND JETSAM \u2013 NO PLACE FOR DISGRACE 2A MA NM.NM.", "Price": "35.00", "Format": "LP"}
